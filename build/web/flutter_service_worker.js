59f1c310359f373d2478006373bfec8fbaed5d51
Leo-devv <mrmr2008mj@gmail.com>
2024-08-05 01:29:28 +0200

    Deploy to gh-pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -52,7 +52,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "aae9bfe6f507406165cf94bbb0b2c9fc
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "3b8d8a3f5d2981868d66a300be029dcd",
+"flutter_bootstrap.js": "daa376f5203007c96dcf31bac77ad213",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
